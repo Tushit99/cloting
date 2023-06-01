cb1addc8145daecaea5a429fc91a2d6d8aa07721
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-06-02 01:54:37 +0530

    auth-page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,16 @@
 import { useSession, signIn, signOut } from "next-auth/react"; 
 
 
-export default function Home() { 
+export default function Home() {
   const { data: session } = useSession()
-  if(!session) { 
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button className="bg-white p-2">Login with Google</button>
-        </div>
-      </div>
-    ); 
+  if(session) {
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
   }
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
-      Logged in
-    </div>
-  );
-
+  return <>
+    Not signed in <br/>
+    <button onClick={() => signIn()}>Sign in</button>
+  </>
 }
